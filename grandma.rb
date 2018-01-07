e9a39a2e8d4c0191c410397a13a9fd2b1e9398ce
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-07 22:39:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,11 @@
 def speak_to_grandma(phrase)
-  if phrase == "i love you too pumpkin"
+  if phrase == "i love you granma"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else 
-    return 
+    return "NO, NOT SINCE 1938!"
+  end 
+end 
 
-# NO, NOT SINCE 1938!
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
