3035ea806a73110d5c662f1149923f5cba4cd98a
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-07 22:38:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,10 @@
 def speak_to_grandma(phrase)
-  puts "HUH?! SPEAK UP, SONNY!"
-  if phrase == "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "I LOVE YOU TOO PUMPKIN!" || phrase== 
+  if phrase == "i love you too pumpkin"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU TOO PUMPKIN!"
+    
+  else 
+    
 
 # NO, NOT SINCE 1938!
 
